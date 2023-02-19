436d8d3719e5ea71068351b0d6701fc6436ccc74
Felipe Alves <felipeaamodio@gmail.com>
2023-02-19 17:53:27 -0300

    first commit, configuring the project with libs and dependencies

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import Home from './src/screens/Home';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Home />
+    </>
+  );
+}
